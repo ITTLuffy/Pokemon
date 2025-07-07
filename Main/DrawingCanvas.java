0922e81948fa58b33bff5cab228f7d17a85d5f11
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-07 18:34:26 +0200

    Feat: override paintComponents method in DrawingCanvas class

--- Main/DrawingCanvas.java
+++ Main/DrawingCanvas.java
@@ -1,10 +1,17 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DrawingCanvas extends JPanel {
     // empty constructor
     public DrawingCanvas() {
 
+    }
+
+    // Override paintComponents method
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
     }
 }

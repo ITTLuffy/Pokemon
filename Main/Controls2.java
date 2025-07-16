568547e2e318570c4f0364d32210cdae2a774073
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-16 13:41:34 +0200

    Feat: add Controls2 panel with custom graphics and controls display

---
+++ Main/Controls2.java
@@ -0,0 +1,53 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Controls2 extends JPanel {
+    public Controls2() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Sfondo azzurro
+        g.setColor(new Color(0, 115, 181));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        // Striscia che separa in alto
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, 60, getWidth(), 8);
+        g.setColor(new Color(0, 160, 209));
+        g.fillRect(0, 68, getWidth(), 3);
+
+        // Effetto finale in basso
+        g.setColor(new Color(0, 106, 122));
+        g.fillRect(0, getHeight() - 13, getWidth(), 5);
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, getHeight() - 8, getWidth(), 8);
+
+        // Scritta controls in alto a sinistra
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("CONTROLS", 12, 48);
+        g.setColor(Color.white);
+        g.drawString("CONTROLS", 10, 45);
+
+        // Scritta next con simbolino della A
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("NEXT", 860, 48);
+        g.setColor(Color.white);
+        g.drawString("NEXT", 858, 45);
+
+        // A
+        g.setFont(new Font("Courier New", Font.BOLD, 30));
+        g.setColor(Color.white);
+        g.fillOval(800, 11, 40, 40);
+        g.setColor(Color.gray);
+        g.drawString("A", 816, 40);
+
+    }
+}

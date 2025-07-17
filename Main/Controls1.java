37ea573d5f3a7a1f048ca12a721299e314cbe582
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-17 11:16:00 +0200

    Feat: enhance key listener in Controls1 for improved focus handling and transition

--- Main/Controls1.java
+++ Main/Controls1.java
@@ -6,6 +6,8 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Controls1 extends JPanel {
+
+
     public Controls1() {
         setFocusable(true); // ricevo input
         requestFocusInWindow(); // richiedo il focus quando viene mostrato
@@ -14,10 +16,10 @@ public class Controls1 extends JPanel {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
-                char key = e.getKeyChar();
+                char key = e.getKeyChar(); // ottengo il carattere premuto
 
-                if (key == 'A' || key == 'a') {
-                    // Rimuovo il pannello dei controlli e aggiungo il prossimo
+                if (key == 'a' || key == 'A') { // se è premuto A o a
+                    // Rimuovo il pannello home e aggiungo il prossimo
                     JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(Controls1.this);
                     frame.getContentPane().remove(Controls1.this);
                     Controls2 c2 = new Controls2(); // creo il prossimo pannello dei controlli
@@ -25,10 +27,15 @@ public class Controls1 extends JPanel {
                     frame.revalidate();
                     frame.repaint(); // forza il ridisegno
                 }
-
             }
         });
 
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        requestFocusInWindow(); // il focus funziona meglio qui
     }
 
     @Override

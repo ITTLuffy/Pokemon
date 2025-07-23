f6c8e61e86dc05ef77da434bf7ba07f2a89403fa
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-23 10:55:47 +0200

    Feat: add Help panel with custom graphics and layout

---
+++ Main/Help.java
@@ -0,0 +1,69 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Help extends JPanel {
+    public Help() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Sfondo azzurro
+        g.setColor(new Color(0, 115, 181));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        // Striscia che separa in alto
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, 60, getWidth(), 8);
+        g.setColor(new Color(0, 160, 209));
+        g.fillRect(0, 68, getWidth(), 3);
+
+        // Effetto finale in basso
+        g.setColor(new Color(0, 106, 122));
+        g.fillRect(0, getHeight() - 13, getWidth(), 5);
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, getHeight() - 8, getWidth(), 8);
+
+        // Scritta controls in alto a sinistra
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("HELP", 12, 48);
+        g.setColor(Color.white);
+        g.drawString("HELP", 10, 45);
+
+        // Scritta next con simbolino della A
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("", 860, 48);
+        g.setColor(Color.white);
+        g.drawString("", 858, 45);
+
+        // A
+        g.setFont(new Font("Courier New", Font.BOLD, 30));
+        g.setColor(Color.white);
+        g.fillOval(800, 11, 40, 40);
+        g.setColor(Color.gray);
+        g.drawString("A", 812, 40);
+
+        g.setFont(new Font("Courier New", Font.BOLD, 30));
+        g.setColor(Color.white);
+        g.fillOval(850, 11, 40, 40);
+        g.setColor(Color.gray);
+        g.drawString("B", 850, 40);
+
+
+        // Spiegazione al centro --> background grigio
+        g.setFont(new Font("Courier New", Font.BOLD, 41));
+        g.setColor(Color.gray);
+
+
+        g.setColor(Color.white);
+        // Bianco
+
+
+    }
+
+}

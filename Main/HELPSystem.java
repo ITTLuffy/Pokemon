87ee7198b83140f37f37683a3456e2326ee4a5d7
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-28 11:32:46 +0200

    Feat: enhance HELPSystem UI with custom graphics and controls

--- Main/HELPSystem.java
+++ Main/HELPSystem.java
@@ -1,6 +1,7 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class HELPSystem extends JPanel {
@@ -26,6 +27,49 @@ public class HELPSystem extends JPanel {
     @Override
     public void paintComponent(java.awt.Graphics g) {
         super.paintComponent(g);
+
+        // Sfondo azzurro
+        g.setColor(new Color(0, 115, 181));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        // Striscia che separa in alto
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, 60, getWidth(), 8);
+        g.setColor(new Color(0, 160, 209));
+        g.fillRect(0, 68, getWidth(), 3);
+
+        // Effetto finale in basso
+        g.setColor(new Color(0, 106, 122));
+        g.fillRect(0, getHeight() - 13, getWidth(), 5);
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, getHeight() - 8, getWidth(), 8);
+
+        // Scritta controls in alto a sinistra
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("HELP", 12, 48);
+        g.setColor(Color.white);
+        g.drawString("HELP", 10, 45);
+
+        // Scritta ok con simbolino della A
+        g.setFont(new Font("Courier New", Font.BOLD, 50));
+        g.setColor(Color.gray);
+        g.drawString("CANCEL", 780, 48);
+        g.setColor(Color.white);
+        g.drawString("CANCEL", 778, 45);
+
+        g.setFont(new Font("Courier New", Font.BOLD, 30));
+        g.setColor(Color.white);
+        g.fillOval(670, 11, 40, 40);
+        g.setColor(Color.gray);
+        g.drawString("A", 682, 40);
+
+        g.setFont(new Font("Courier New", Font.BOLD, 30));
+        g.setColor(Color.white);
+        g.fillOval(720, 11, 40, 40);
+        g.setColor(Color.gray);
+        g.drawString("B", 732, 40);
+
 
     }
 }

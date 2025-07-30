c2951ed581c41b200fca608faa35df603c026e40
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-30 16:40:25 +0200

    Feat: add key handling in TheGame to transition to Help3 on 'A' and 'B' key press

--- Main/TheGame.java
+++ Main/TheGame.java
@@ -5,7 +5,38 @@ import java.awt.*;
 
 public class TheGame extends JPanel {
     public TheGame() {
+        setFocusable(true); // ricevo input
+        requestFocusInWindow(); // richiedo il focus quando viene mostrato
 
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyPressed(java.awt.event.KeyEvent e) {
+                super.keyPressed(e);
+                char keyChar = e.getKeyChar(); // ottengo il carattere premuto
+
+                if (keyChar == 'A' || keyChar == 'a') { // Se è premuto A
+                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(TheGame.this);
+                    frame.getContentPane().remove(TheGame.this); // Rimuovo il pannello dei controlli
+                    Help3 h3 = new Help3(); // creo il prossimo pannello del gioco
+                    frame.getContentPane().add(h3); // aggiungo il nuovo pannello
+                    frame.revalidate(); // rendo visibile il nuovo pannello
+                } else if (keyChar == 'B' || keyChar == 'b') { // Se è premuto B
+                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(TheGame.this);
+                    frame.getContentPane().remove(TheGame.this); // Rimuovo il pannello dei controlli
+                    Help3 h3 = new Help3(); // creo il prossimo pannello della home page
+                    frame.getContentPane().add(h3); // aggiungo il nuovo pannello
+                    frame.revalidate(); // rendo visibile il nuovo pannello
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        requestFocusInWindow(); // il focus funziona meglio qui
     }
 
     @Override

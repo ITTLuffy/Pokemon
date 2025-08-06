14457b69ad9318ddd2eb0cb450ede2a3f0096a68
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-08-06 16:45:25 +0200

    Feat: enhance Controls3 class with focus handling and custom painting

--- Main/Controls3.java
+++ Main/Controls3.java
@@ -1,7 +1,23 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Controls3 extends JPanel {
+    public Controls3() {
+
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        requestFocusInWindow(); // il focus funziona meglio qui
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+    }
 
 }

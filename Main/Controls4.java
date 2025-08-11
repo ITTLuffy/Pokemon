f3150774d46531901b8700181a6f4c202f0768cc
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-08-11 13:47:14 +0200

    Feat: update color and dimensions in Controls4 custom painting

--- Main/Controls4.java
+++ Main/Controls4.java
@@ -21,8 +21,11 @@ public class Controls4 extends JPanel {
         g.fillRect(0, 60, getWidth(), 11);
 
         // Striscia blu violaceo
-        g.setColor(new Color(71, 99, 181));
-        g.fillRect(0, 60, getWidth(), 11);
+        g.setColor(new Color(82, 99, 181));
+        g.fillRect(0, 71, getWidth(), 35);
+        g.fillRect(0, 106, 4, getHeight());
+        g.fillRect(getWidth() - 4, 106, 4, getHeight());
+        g.fillRect(0, getHeight() - 4, getWidth(), 4);
 
 
     }

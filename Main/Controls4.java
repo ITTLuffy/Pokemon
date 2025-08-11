df8a65dc6b5f2eae28f5ee64b90b43e1884ff9aa
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-08-11 13:40:28 +0200

    Feat: implement custom painting in Controls4 class

--- Main/Controls4.java
+++ Main/Controls4.java
@@ -1,4 +1,29 @@
 package Main;
 
-public class Controls4 {
+import javax.swing.*;
+import java.awt.*;
+
+public class Controls4 extends JPanel {
+    public Controls4() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Sfondo azzurro
+        g.setColor(new Color(0, 115, 181));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        // Striscia scura
+        g.setColor(new Color(0, 77, 107));
+        g.fillRect(0, 60, getWidth(), 11);
+
+        // Striscia blu violaceo
+        g.setColor(new Color(71, 99, 181));
+        g.fillRect(0, 60, getWidth(), 11);
+
+
+    }
 }

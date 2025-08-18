69543e08513cc9f0a2d545d18c6d42701ed745b2
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-08-18 15:06:53 +0200

    Feat: implement key listener for panel transitions and load images

--- Main/Pres1.java
+++ Main/Pres1.java
@@ -1,7 +1,50 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class Pres1 extends JPanel{
+public class Pres1 extends JPanel {
+    private Image freccia = null;
+    private Image oak = null;
+
+    public Pres1() {
+        setFocusable(true); // ricevo input
+        requestFocusInWindow(); // richiedo il focus quando viene mostrato
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                char key = e.getKeyChar(); // ottengo il carattere premuto
+
+                if (key == 'a' || key == 'A') { // se è premuto A o a
+                    // Rimuovo il pannello home e aggiungo il prossimo
+                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(Pres1.this);
+                    frame.getContentPane().remove(Pres1.this);
+                    Controls5 c3 = new Controls5(); // creo il prossimo pannello dei controlli
+                    frame.getContentPane().add(c3); // aggiungo il nuovo pannello
+                    frame.revalidate();
+                    frame.repaint(); // forza il ridisegno
+                } else if (key == 'b' || key == 'B') {
+                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(Pres1.this);
+                    frame.getContentPane().remove(Pres1.this);
+                    Controls5 c2 = new Controls5(); // creo il pannello dei controlli
+                    frame.getContentPane().add(c2); // aggiungo il nuovo pannello
+                    frame.revalidate();
+                    frame.repaint(); // forza il ridisegno
+                }
+            }
+        });
+
+        try {
+            oak = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Entities/Tutorial/Oak.png"));
+            freccia = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Entities/Tutorial/Arrow.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 
 }
